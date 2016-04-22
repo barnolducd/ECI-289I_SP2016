60ce5a725944e6a0c1056f00bd9a701fca6e106f
Brad Arnold <barnold@ucdavis.edu>
2016-04-22 12:48:00 -0700

    HW 3 Intro.

--- L5-ES-mcommal (1).py
+++ L5-ES-mcommal (1).py
@@ -6,23 +6,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# function to optimize
+# Define MatLab "Peaks" Function
 def peaks(x):
   a = 3*(1-x[0])**2*np.exp(-(x[0]**2) - (x[1]+1)**2)
   b = 10*(x[0]/5 - x[0]**3 - x[1]**5)*np.exp(-x[0]**2-x[1]**2)
   c = (1/3)*np.exp(-(x[0]+1)**2 - x[1]**2)
-  return a - b - c + 6.551 # add this so objective is always positive
+  return a - b - c + 6.551
 
 ub = 3.0
 lb = -3.0
 
-d = 2 # dimension of decision variable space
-s = 0.5 # stdev of normal noise (if this is too big, it's just random search!)
+d = 2 # Dimension of Decision Variable Space
+s = 0.5 # Standard Deviation of Normal Noise
 num_seeds = 10
 
 m = 1
-l = 100 # beware "lambda" is a reserved keyword
-max_NFE = 60000 # should be a multiple
+l = 100
+max_NFE = 60000
 ft = np.zeros((num_seeds, max_NFE/l))
 
 def mutate(x, lb, ub, sigma):
@@ -31,33 +31,33 @@ def mutate(x, lb, ub, sigma):
     x_trial = x + np.random.normal(0, sigma, x.size)
   return x_trial
 
-# (mu,lambda) evolution strategy
+# (mu,lambda) Evolution Strategy
 for seed in range(num_seeds):
   np.random.seed(seed)
 
-  # random initial population (l x d matrix)
+  # Random Initial Population (l x d matrix)
   P = np.random.uniform(lb, ub, (l,d))
-  f = np.zeros(l) # we'll evaluate them later
+  f = np.zeros(l)
   nfe = 0
   f_best, x_best = None, None
 
   while nfe < max_NFE:
 
-    # evaluate all solutions in the population
+    # Evaluate All Solutions in Population
     for i,x in enumerate(P):
       f[i] = peaks(x)
       nfe += 1
 
-    # find m best parents, truncation selection
+    # Find m "Best" Parents, Truncation Selection
     ix = np.argsort(f)[:m]
     Q = P[ix, :]
 
-    # keep track of best here
+    # Keep Track of "Best"
     if f_best is None or f[ix[0]] < f_best:
       f_best = f[ix[0]]
       x_best = Q[0,:]
 
-    # then mutate: each parent generates l/m children (integer division)
+    # Mutate: Each Parent Generates l/m Children (Integer Division)
     child = 0
     for i,x in enumerate(Q):
       for j in range(int(l/m)):
@@ -66,11 +66,10 @@ for seed in range(num_seeds):
 
     ft[seed,nfe/l-1] = f_best
 
-  # for each trial print the result (but the traces are saved in ft)
   print x_best
   print f_best
 
-# add a line for "enumeration"
+# Define Enumeration Line
 enumy = np.arange(2.0,-3.0,-1.0)
 enumx = (10**(-enumy)*(ub-lb))**d
 
